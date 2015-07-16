e10e39064113073612f88a1aca3e50fb9c8a8aef
ssddttaa <sadatalishaik@gmail.com>
2015-07-16 13:41:58 -0400

    recommitting old commit

---
+++ catkin_ws/build/mesh_map-master/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;sensor_msgs;image_transport;cv_bridge;tf".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "mesh_map"
+PROJECT_SPACE_DIR = "/home/mrsl_student/git/catkin_ws/devel"
+PROJECT_VERSION = "0.0.0"
