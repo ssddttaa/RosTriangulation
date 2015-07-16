37b1e45f882ab4d77ca00721e794d20a61e35d0b
ssddttaa <sadatalishaik@gmail.com>
2015-07-16 11:44:12 -0400

    Fixed launch file, removed build and devel folders

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mrsl_student/git/catkin_ws/src
-# Build directory: /home/mrsl_student/git/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(mesh_map-master)
